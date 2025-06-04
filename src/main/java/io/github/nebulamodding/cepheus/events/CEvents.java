d51b4f7a42de5c218163d03bdef1c1b6d50c8f0f
DylThePill <118853769+AveryHazel@users.noreply.github.com>
2025-06-04 17:13:01 -0400

    Add shitty frigus dirt to mud event

--- src/main/java/io/github/nebulamodding/cepheus/events/CEvents.java
+++ src/main/java/io/github/nebulamodding/cepheus/events/CEvents.java
@@ -1,24 +1,45 @@
 package io.github.nebulamodding.cepheus.events;
 
+import io.github.nebulamodding.cepheus.registry.CTags;
 import io.github.nebulamodding.cepheus.registry.block.CBlocks;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.ItemAbilities;
 import net.neoforged.neoforge.common.ItemAbility;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
+import static net.minecraft.core.component.DataComponents.POTION_CONTENTS;
 import static net.neoforged.neoforge.common.NeoForge.EVENT_BUS;
 
 public class CEvents {
     public static void gatherEvents(IEventBus bus) {
         bus.addListener(CEvents::setup);
-        EVENT_BUS.addListener(CEvents::blockItemInteractions);
+        EVENT_BUS.addListener(CEvents::blockToolInteractions);
+        EVENT_BUS.addListener(CEvents::useOn);
     }
+
     private static void setup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(CBlocks.MAYURA_SAPLING.getId(), CBlocks.POTTED_MAYURA_SAPLING);
@@ -28,10 +49,10 @@ public class CEvents {
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(CBlocks.ICEFLOWER.getId(), CBlocks.POTTED_ICEFLOWER);
         });
     }
-    private static void blockItemInteractions(BlockEvent.BlockToolModificationEvent event) {
+
+    private static void blockToolInteractions(BlockEvent.BlockToolModificationEvent event) {
         ItemAbility action = event.getItemAbility();
         BlockState keepState = event.getState();
-        BlockState keepButtonState = event.getState();
         UseOnContext context = event.getContext();
         if (!event.isSimulated()) {
             if (action == ItemAbilities.SHOVEL_FLATTEN && (context.getClickedFace() != Direction.DOWN && context.getLevel().getBlockState(context.getClickedPos().above()).isAir())) {
@@ -62,5 +83,40 @@ public class CEvents {
                 }
             }
         }
+    }
+
+    public static InteractionResult useOn(PlayerInteractEvent.RightClickBlock event) {
+        Level level = event.getLevel();
+        BlockPos blockPos = event.getPos();
+        Player player = event.getEntity();
+        ItemStack itemStack = player.getItemInHand(event.getHand());
+        PotionContents potionContents = itemStack.getOrDefault(POTION_CONTENTS, PotionContents.EMPTY);
+        BlockState clickedBlock = level.getBlockState(blockPos);
+        if (clickedBlock.is(CTags.CBlockTags.CONVERTABLE_TO_FRIGUS_MUD) && potionContents.is(Potions.WATER)) {
+                level.playSound(null, blockPos, SoundEvents.GENERIC_SPLASH, SoundSource.BLOCKS, 1.0F, 1.0F);
+                player.setItemInHand(event.getHand(), ItemUtils.createFilledResult(itemStack, player, new ItemStack(Items.GLASS_BOTTLE)));
+                player.awardStat(Stats.ITEM_USED.get(itemStack.getItem()));
+                if (!level.isClientSide()) {
+                    ServerLevel serverlevel = (ServerLevel) level;
+                    for (int i = 0; i < 5; i++) {
+                        serverlevel.sendParticles(
+                                ParticleTypes.SPLASH,
+                                (double) blockPos.getX() + level.random.nextDouble(),
+                                blockPos.getY() + 1,
+                                (double) blockPos.getZ() + level.random.nextDouble(),
+                                1,
+                                0.0,
+                                0.0,
+                                0.0,
+                                1.0
+                        );
+                    }
+                }
+                level.playSound(null, blockPos, SoundEvents.BOTTLE_EMPTY, SoundSource.BLOCKS, 1.0F, 1.0F);
+                level.gameEvent(null, GameEvent.FLUID_PLACE, blockPos);
+                level.setBlockAndUpdate(blockPos, CBlocks.FRIGUS_MUD.get().defaultBlockState());
+                return InteractionResult.sidedSuccess(level.isClientSide);
+            }
+        return InteractionResult.PASS;
     }
 }
